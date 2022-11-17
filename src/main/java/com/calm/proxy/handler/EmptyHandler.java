d8468bd47e23a30eb98521dea0f4da3b452ed1d9
calm0406 <calm0406@163.com>
2022-11-17 22:30:29 +0800

    test

--- src/main/java/com/calm/proxy/handler/EmptyHandler.java
+++ src/main/java/com/calm/proxy/handler/EmptyHandler.java
@@ -1,5 +1,6 @@
 package com.calm.proxy.handler;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.FullHttpResponse;
@@ -10,6 +11,8 @@ public class EmptyHandler extends SimpleChannelInboundHandler<FullHttpResponse> 
     protected void channelRead0(ChannelHandlerContext ctx, FullHttpResponse msg) {
         //修改http响应体返回至客户端
         msg.headers().add("test1111","from proxy");
+        ByteBuf content = msg.content();
+        System.out.println(content);
 //        clientChannel.writeAndFlush(msg);
     }
 }
--- src/main/java/com/calm/proxy/proxy/DumpCreatePlanProxyHandler.java
+++ src/main/java/com/calm/proxy/proxy/DumpCreatePlanProxyHandler.java
@@ -3,10 +3,12 @@ package com.calm.proxy.proxy;
 import com.calm.proxy.ProxyHandler;
 import com.calm.proxy.handler.EmptyHandler;
 import com.calm.proxy.listener.AfterConnectionListener;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpObjectAggregator;
+import org.springframework.util.StringUtils;
 
 import java.net.URI;
 
@@ -16,6 +18,9 @@ public class DumpCreatePlanProxyHandler implements ProxyHandler {
     @Override
     public boolean isSupport(FullHttpRequest request) {
         String path = URI.create(request.uri()).getPath();
+        if (!StringUtils.hasText(path)) {
+            return false;
+        }
         return path.contains("/plan");
     }
 
@@ -25,7 +30,8 @@ public class DumpCreatePlanProxyHandler implements ProxyHandler {
 
         modifyUser(headers, "123456");
         //创建客户端连接目标机器
-        connectToRemote(ctx, URI.create(request.uri()).getHost(), 80, 10000,new HttpObjectAggregator(1024 * 1024 * 1024), new EmptyHandler()).addListener(new AfterConnectionListener(ctx, request, headers));
+        ChannelFuture channelFuture = connectToRemote(ctx, URI.create(request.uri()).getHost(), 80, 10000, new HttpObjectAggregator(1024 * 1024 * 1024), new EmptyHandler());
+        channelFuture.addListener(new AfterConnectionListener(ctx, request, headers));
     }
 
 
